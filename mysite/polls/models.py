0cac4ddce63dc65ce1c51af351568fb0191e0674
Snoop Roggie Rodge <roger.mcnall001@umb.edu>
2015-12-17 05:32:25 -0800

    Created Admin Site and Complete Part 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -31,5 +33,4 @@ class Question(models.Model):
     # ...
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
 
