40464202311faa86fec8687f1c281aa1adb43690
Snoop Roggie Rodge <roger.mcnall001@umb.edu>
2015-12-17 05:01:37 -0800

    Finished Part 1

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,8 +4,32 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) Create your models here.
+    votes = models.IntegerField(default=0)
+
+from django.db import models
+
+class Question(models.Model):
+    # ...
+    def __str__(self):              # __unicode__ on Python 2
+        return self.question_text
+
+class Choice(models.Model):
+    # ...
+    def __str__(self):              # __unicode__ on Python 2
+        return self.choice_text
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    # ...
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
