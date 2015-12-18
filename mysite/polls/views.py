052fadc5b6d96e3f2e84a6e2f07b6e3943a964f7
Snoop Roggie Rodge <roger.mcnall001@umb.edu>
2015-12-17 17:43:01 -0800

    edit views.py made edit to views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
-
+from django.template import RequestContext, loader
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -14,5 +14,8 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
