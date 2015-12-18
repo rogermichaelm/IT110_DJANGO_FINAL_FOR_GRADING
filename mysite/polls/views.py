f486c37d009fc51d40298c386dee7248b608bcea
Snoop Roggie Rodge <roger.mcnall001@umb.edu>
2015-12-17 19:02:42 -0800

    polls/views vote view add added additinal code to enable view that redirects to the results page for question

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -21,3 +21,10 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+from django.shortcuts import get_object_or_404, render
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
